0ac5d599d47550bd3d5649a52b8ccc9b46fa014a
David Silwal <de.davidsilwal@hotmail.com>
2019-01-15 18:59:26 +0545

    Default redirect to HealthCheck-UI

--- HealthCheck/Startup.cs
+++ HealthCheck/Startup.cs
@@ -27,11 +27,6 @@ namespace HealthCheck
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
-            //services
-            //    .AddHealthChecks()
-            //    .AddCheck(new SqlConnectionHealthCheck("MyDatabase", Configuration["ConnectionStrings:DefaultConnection"]));
-
             services
                 .AddHealthChecks()
                 .AddOracle("")
@@ -43,26 +38,6 @@ namespace HealthCheck
                     failureStatus: HealthStatus.Degraded,
                     tags: new string[] { "db", "s   ql", "sqlserver" })
                 .AddRabbitMQ("");
-
-            ;
-
-            //    .AddUrlGroup(new Uri("https://api.simplecast.com/v1/podcasts.json"), "Simplecast API", HealthStatus.Degraded)
-
-            //.AddSqlServer(
-            //    connectionString: Configuration["Data:ConnectionStrings:Sql"],
-            //    healthQuery: "SELECT 1;",
-            //    name: "sql",
-            //    failureStatus: HealthStatus.Degraded,
-            //    tags: new string[] { "db", "sql", "sqlserver" });
-
-            //services.AddSingleton<SomeDependency>();
-
-
-            // register the custom health check 
-            // after AddHealthChecks and after SomeDependency 
-            //  services.AddSingleton<IHealthCheck, SomeHealthCheck>();
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -75,37 +50,16 @@ namespace HealthCheck
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
-
-            //  app.UseHealthChecks("/healthz");
 
             app.UseHealthChecksUI(option => { option.UIPath = "/hz"; });
 
-
-            //app.UseHealthChecks("/hc",
-            //    new HealthCheckOptions
-            //    {
-            //        ResponseWriter = async (context, report) =>
-            //        {
-            //            var result = JsonConvert.SerializeObject(
-            //                new
-            //                {
-            //                    status = report.Status.ToString(),
-            //                    errors = report.Entries.Select(e => new { key = e.Key, value = Enum.GetName(typeof(HealthStatus), e.Value.Status) })
-            //                });
-            //            context.Response.ContentType = MediaTypeNames.Application.Json;
-            //            await context.Response.WriteAsync(result);
-            //        }
-            //    });
-
-
-            app.UseMvc();
+            app.Use(async (context, next) =>
+            {
+                context.Response.Redirect("/hz");
+            });
         }
     }
 }
